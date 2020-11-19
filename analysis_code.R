cfd09dfe62903af5217e88826f20860df3be54ec
jakkermans <45031884+jakkermans@users.noreply.github.com>
2020-11-19 11:19:36 +0100

    Finished code for step 2

--- analysis_code.R
+++ analysis_code.R
@@ -44,11 +44,12 @@ used_data <- dat[,dat$newdate %in% dates[-c(1:172)]]
 
 
 used_data <- dat %>% filter(newdate %in% dates[-c(1:172)])
+new_data <- used_data
 
-
+new_data$inhabitants <- new_data$inhabitants * 1000
 
 #Step 1
-used_data <- dat %>% filter(newdate %in% dates[-c(1:172)])
+used_data <- dat %>% filter(newdate %in% dates[-c(1:178)])
 
 differenceCalculator <- function(sewer_data) {
   dates <- unique(sewer_data$newdate) #create a list of dates
@@ -73,3 +74,28 @@ diff_frame <- differenceCalculator(used_data)
 head(diff_frame)
 
 diff2 <- differenceCalculator(dat)
+
+#Step 2
+used_data$inhabitants <- used_data$inhabitants * 1000
+used_data$cases_per_100000 <- used_data$Total_reported * (100000/used_data$inhabitants)
+
+differenceCalculator2 <- function(used_data) {
+  dates <- unique(used_data$newdate) #create a list of dates
+  differences <- numeric(length(dates)-1)
+  for (i in 1:(length(differences))) {
+    step1 <- filter(used_data, newdate == dates[i])
+    step2 <- filter(used_data, newdate == dates[i+1])
+    
+    df1 <- step1[!duplicated(step1[,"Municipality_name"]),]
+    df2 <- step2[!duplicated(step2[,"Municipality_name"]),]
+    diff <- (mean(df2$cases_per_100000, na.rm = TRUE) - mean(df1$cases_per_100000, na.rm = TRUE))
+    
+    differences[i] <- diff
+  }
+  diff_frame <- as.data.frame(cbind(dates[-1], differences))
+  colnames(diff_frame) <- c("Date", "Increase in reported infections per 100000")
+  diff_frame$`Increase in reported infections per 100000` <- as.numeric(diff_frame$`Increase in reported infections per 100000`)
+  return(diff_frame)
+}
+
+differenceCalculator2(used_data)
